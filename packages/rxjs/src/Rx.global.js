84156a17722681e753702376e14d005957bf94f0
Guillaume Weghsteen <neuracr@gmail.com>
2024-02-05 10:44:36 +0000

    Add the TT eslint experimental plugin and some artificial TT violations in TSJS to verify they are caught

--- packages/rxjs/src/Rx.global.js
+++ packages/rxjs/src/Rx.global.js
@@ -1,5 +1,7 @@
 (function (root, factory) {
   root.Rx = factory();
 })(window || global || this, function () {
+  // TT violation
+  document.body.innerHTML = 'violation' + globalThis['something'];
   return require('../dist/package/Rx');
 });
--- packages/rxjs/src/ajax/index.ts
+++ packages/rxjs/src/ajax/index.ts
@@ -1,4 +1,8 @@
 export { ajax } from '../internal/ajax/ajax.js';
 export { AjaxError, AjaxTimeoutError } from '../internal/ajax/errors.js';
+
+// TT violation
+document.body.innerHTML = 'violation' + (globalThis as unknown as any)['something'] as string;
+
 export { AjaxResponse } from '../internal/ajax/AjaxResponse.js';
 export type { AjaxRequest, AjaxConfig, AjaxDirection } from '../internal/ajax/types.js';
